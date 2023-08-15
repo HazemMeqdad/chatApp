b545fc9663b50a710e94bd523dfec583e52d72ea
hazemmeqdad <hazemmeqdad@outlook.com>
2023-08-15 10:08:52 +0300

    Models

--- ws.js
+++ ws.js
@@ -1,7 +1,12 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket({ port: 8080 });
+const wss = new WebSocket();
 
 wss.on('connection', ws => {
     console.log('New client connected!');
+    ws.send("Welcome New Client! Please provide your id in local storage to connect to the server.")
+    ws.on('message', message => {
+        console.log(`Received message => ${message}`)
+        ws.send('Hello World!');
+    })
 })
