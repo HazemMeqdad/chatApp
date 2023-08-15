be3b688e1a55257612ed8dc74d487034dbbdff1e
hazemmeqdad <hazemmeqdad@outlook.com>
2023-08-15 10:14:13 +0300

    Fix an cors problem

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cros = require('cros');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cros());
+app.use(cors());
 
 
 
